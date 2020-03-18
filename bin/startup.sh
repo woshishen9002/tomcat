723740c3212d7ae64366b54b9af4545ea099a701
woshishen9002 <952092838@qq.com>
2020-03-18 20:41:32 +0800

    1.添加运行时依赖jar lib/* 2.startup.sh 添加注释

--- bin/startup.sh
+++ bin/startup.sh
@@ -21,13 +21,16 @@
 
 # Better OS/400 detection: see Bugzilla 31132
 os400=false
+#显示系统信息
 case "`uname`" in
 OS400*) os400=true;;
 esac
 
 # resolve links - $0 may be a softlink
+#Shell本身的文件名
 PRG="$0"
 
+#-h filename 如果文件是软链接，则为真
 while [ -h "$PRG" ] ; do
   ls=`ls -ld "$PRG"`
   link=`expr "$ls" : '.*-> \(.*\)$'`
@@ -38,17 +41,21 @@ while [ -h "$PRG" ] ; do
   fi
 done
 
+#该文件的文件夹目录
 PRGDIR=`dirname "$PRG"`
 EXECUTABLE=catalina.sh
 
 # Check that target executable exists
+# 判断是否是IBM的AIX系统
 if $os400; then
   # -x will Only work on the os400 if the files are:
   # 1. owned by the user
   # 2. owned by the PRIMARY group of the user
   # this will not work if the user belongs in secondary groups
+  #用于两次定向
   eval
 else
+  #-x filename 如果 filename可执行，则为真 ，！取反
   if [ ! -x "$PRGDIR"/"$EXECUTABLE" ]; then
     echo "Cannot find $PRGDIR/$EXECUTABLE"
     echo "The file is absent or does not have execute permission"
@@ -57,4 +64,5 @@ else
   fi
 fi
 
+#$@ 是传给脚本的所有参数的列表
 exec "$PRGDIR"/"$EXECUTABLE" start "$@"
